6335adc4c9e594a9d24d1c0fa12864809932a1ae
Omer <omerfarukdasar@gmail.com>
2022-06-25 21:25:19 +0200

    all done

--- weatherapp/admin.py
+++ weatherapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
+from .models import City
 
-from weatherapp.models import City
-
+# Register your models here.
 admin.site.register(City)
--- weatherapp/models.py
+++ weatherapp/models.py
@@ -1,11 +1,8 @@
 from django.db import models
 
-
+# Create your models here.
 class City(models.Model):
-    class Meta:
-        verbose_name_plural = "Cities"
-
+    name = models.CharField(max_length=100)
+    
     def __str__(self):
         return self.name
-
-    name = models.CharField(max_length=100)
