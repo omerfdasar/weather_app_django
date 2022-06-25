a47b650320c4d0268ee251d860e905dc09ffa188
Omer <omerfarukdasar@gmail.com>
2022-06-25 18:00:19 +0200

    API added

--- weatherapp/models.py
+++ weatherapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    class Meta:
+        verbose_name_plural = "Cities"
+
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=100)
--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from decouple import config
+import requests
+from pprint import pprint
 
-def home(request):    
-    return render(request, "weatherapp/home.html")
+
+def home(request):
+    API_KEY = config('API_KEY')
+    city = "Antwerp"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    response = requests.get(url)
+    content = response.json()
+
+    context = {
+        'city':  content['name'],
+        'temp':   content['main']['temp'],
+        'icon':   content['weather'][0]['icon'],
+        'desc':   content['weather'][0]['description'],
+    }
+
+    return render(request, "weatherapp/home.html", context)
